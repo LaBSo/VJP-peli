47fa5bb6c351f1c074ba211d59b81b5f26bad99e
LaBSo <jooel.friman@gmail.com>
2014-04-10 12:18:00 +0300

    Raakile koodi

---
+++ car.js
@@ -0,0 +1,24 @@
+
+function car ={
+	this.speed;
+	this.gear;
+	this.location;
+	this.kierrokset;
+}
+function accelerate{
+ /*
+  * Kiihdyttäminen ja vaihteen vaihtaminen
+  */
+}
+
+function changeGear {
+	/*
+	 * Vaihteen vaihtaminen
+	 */
+}
+
+function brake {
+	/*
+	 * Hidastaminen
+	 */
+}
