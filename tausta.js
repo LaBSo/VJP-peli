84f56d07c47fbdd2ad6b144d60355f7c4c75ca94
myrsyli <rantanen.johanna@hotmail.com>
2014-05-09 13:41:22 +0300

    jeee tausta toimii!!

--- tausta.js
+++ tausta.js
@@ -22,11 +22,11 @@ tausta.prototype.drawBasic= function(x) {
 }
  
 tausta.prototype.drawShort= function(x) {
-   ctx.drawImage(this.img, 0, 500, 100, gameHeight, newX, 0, 100, gameHeight);
+   ctx.drawImage(this.img, 0, 500, 100, gameHeight, x, 0, 100, gameHeight);
 }
  
 tausta.prototype.drawHigh= function(x) {
-   ctx.drawImage(this.img, 0, 2000, 300, gameHeight, newX, 0, 300, gameHeight);
+   ctx.drawImage(this.img, 0, 2000, 300, gameHeight, x, 0, 300, gameHeight);
 }
  
 tausta.prototype.drawLow= function(x) {
@@ -34,36 +34,60 @@ tausta.prototype.drawLow= function(x) {
 }
  
 tausta.prototype.drawGround= function(x) {
-   ctx.drawImage(this.img, 0, 0, gameWidth, gameHeight, newX, 0, gameWidth, gameHeight);
+   ctx.drawImage(this.img, 0, 0, gameWidth, gameHeight, x, 0, gameWidth, gameHeight);
 }
  
 tausta.prototype.drawBackground = function(x){
         ctx.drawImage(this.taustantausta, 0, 0, gameWidth, gameHeight, 0, 0, gameWidth, gameHeight);
        
-        for(var i=0; i < basicHills.length; i++){
+        for(var i=0; i < basicHills.length; i++){ //basicHills
                 var miinustus = this.properX(Math.round(x));
                 if((x+799>= basicHills[i] && x<basicHills[i])){
                     var newX = this.properX(basicHills[i]-miinustus);
                     this.drawBasic(newX);                   
-                } else if(x<= basicHills[i]+this.basicWidth && x>=basicHills[i]){
-                    var erotus = this.properX(basicHills[i]);
-                    this.drawBasic(-miinustus+erotus);               
-                }  else {
-                    return;
-                }
+                } else if(x<= basicHills[i]+this.basicWidth && x>=basicHills[i]){               
+                    var erotus = x-basicHills[i]
+                    this.drawBasic(-erotus);               
+                }  
         }
-        for(var i=0; i < lowHills.length; i++){
+        for(var i=0; i < lowHills.length; i++){ //lowHills
                 var miinustus = this.properX(Math.round(x));
                 if((x+799>= lowHills[i] && x<lowHills[i])){
                     var newX = this.properX(lowHills[i]-miinustus);
-                    this.drawLow(newX); 
-                    console.log(newX);                  
+                    this.drawLow(newX);                  
                 } else if(x<= lowHills[i]+this.lowWidth && x>=lowHills[i]){
-                    console.log(-miinustus);
-                    var erotus = this.properX(lowHills[i]);
-                    this.drawLow(-miinustus+erotus);               
-                }  else {
-                    return;
-                }
+                    var erotus = x-lowHills[i]
+                    this.drawLow(-erotus);               
+                }  
+        }
+        for(var i=0; i < grounds.length; i++){ //long grounds
+                var miinustus = this.properX(Math.round(x));
+                if((x+799>= grounds[i] && x<grounds[i])){
+                    var newX = this.properX(grounds[i]-miinustus);
+                    this.drawGround(newX);                  
+                } else if(x<= grounds[i]+this.normalWidth && x>=grounds[i]){
+                    var erotus = x-grounds[i]
+                    this.drawGround(-erotus);               
+                }  
+        }
+        for(var i=0; i < shortGrounds.length; i++){ //short grounds
+                var miinustus = this.properX(Math.round(x));
+                if((x+799>= shortGrounds[i] && x<shortGrounds[i])){
+                    var newX = this.properX(shortGrounds[i]-miinustus);
+                    this.drawShort(newX);                  
+                } else if(x<= shortGrounds[i]+this.groundWidth && x>=shortGrounds[i]){
+                    var erotus = x-shortGrounds[i]
+                    this.drawShort(-erotus);               
+                }  
+        }
+        for(var i=0; i < highHills.length; i++){ //short grounds
+                var miinustus = this.properX(Math.round(x));
+                if((x+799>= highHills[i] && x<highHills[i])){
+                    var newX = this.properX(highHills[i]-miinustus);
+                    this.drawHigh(newX);                  
+                } else if(x<= highHills[i]+this.highWidth && x>=highHills[i]){
+                    var erotus = x-highHills[i]
+                    this.drawHigh(-erotus);               
+                }  
         }
 }
